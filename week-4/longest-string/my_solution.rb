30a85261c50c6e9e0dacb8a8f7d1fa348cd57f08
Jordan <jordanfox15@hotmail.com>
2015-12-04 01:27:33 +0000

    4.6 challenges

--- week-4/longest-string/my_solution.rb
+++ week-4/longest-string/my_solution.rb
@@ -1,17 +1,18 @@
-# Shortest String
+# Longest String
 
 # I worked on this challenge [by myself].
 
-# shortest_string is a method that takes an array of strings as its input
-# and returns the shortest string
+# longest_string is a method that takes an array of strings as its input
+# and returns the longest string
 #
 # +list_of_words+ is an array of strings
-# shortest_string(array) should return the shortest string in the +list_of_words+
+# longest_string(list_of_words) should return the longest string in +list_of_words+
 #
 # If +list_of_words+ is empty the method should return nil
 
-#Your Solution Below
-def shortest_string(list_of_words)
+
+# Your Solution Below
+def longest_string(list_of_words)
 	if list_of_words[0].nil?
 		return nil
 	else
@@ -21,6 +22,6 @@ def shortest_string(list_of_words)
 			string_length[words] = words.length
 		end
 		sorted = string_length.sort_by{|x, y| y}
-		return sorted.first.first
+		return sorted.last.first
 	end
 end
