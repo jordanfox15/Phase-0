76b19d796f2380b28d4e2ae0e2911570bf09be5f
Jordan <jordanfox15@hotmail.com>
2015-12-12 00:29:22 +0000

    5.4 solution

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -1,10 +1,10 @@
 # Research Methods
 
-# I spent [] hours on this challenge.
+# I spent [1] hour on this challenge.
 
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
-my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
-
+my_family_pets_ages = {"Evi" => 6, "Ditto" => 2, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 5, "Annabelle" => 0}
+=begin
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
   source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
@@ -33,20 +33,21 @@ end
 #
 #
 
-
+=end
 # Person 3
 def my_array_sorting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.sort_by {|x| x.to_s}
 end
 
 def my_hash_sorting_method(source)
-   source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+   source.sort_by{|x, y| y}
 end
 
+=begin
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+I used the method sort_by.  This method takes the input and sorts it by a code block you specify.
+You can see the syntax above.  The variables in the ||'s are the parameters and the code after that is telling the method how to sort.  It could be almost anything depending on how you want to sort and what block of code you use.  In the first example I needed to turn the integers into strings first so sort would be able to compare everything in the array.  In the second I needed to sort by the hash values not the keys.
+I'm sorry but I have no tricks for the Ruby Docs.  I have used this method enough and seen enough examples to know it.  The docs are very frustrating to use.
 
 
 # Person 4
@@ -92,3 +93,4 @@ end
 #
 #
 #
+=end
