385024bd12c931d6deadf26c690b43ac402c33f0
Jordan <jordanfox15@hotmail.com>
2016-01-02 23:35:27 +0000

    7.8 Psuedocode

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,30 @@
+/*
+A.
+	1. Create a function with the name of "sum" that accepts an array of integers as an argument
+
+	2. Add together all the integers of the array to get a total
+
+	3. Return the total
+
+B.
+	1. Create a function with the name of "mean" that accepts the same arrays as arguments
+
+	2. Add together all the integers of the array to get a total
+
+	3. Divide the total by the number of elements to get the average
+
+	4. Return the average
+
+C.
+	1. Create a function with the name of "median" that accepts the same arrays as arguments
+
+	2. Order the array from smallest integer to largest
+
+	3. If the number of elements in the array is odd
+		a. Return the element at the midpoint
+
+	4. If the number of elements in the array is even
+		a. Add together the 2 elements at the midpoint
+		b. Divide total by 2 to get the average
+		c. Return the average
+*/
