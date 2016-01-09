b6ed536f0867e65980751ee74482df47f263d23d
Jordan <jordanfox15@hotmail.com>
2016-01-09 22:05:40 +0000

    8.6 solution

--- week-8/ruby/ruby-review.rb
+++ week-8/ruby/ruby-review.rb
@@ -1,22 +1,20 @@
 # Reverse Words
 
 
-# I worked on this challenge [by myself, with: ].
-# This challenge took me [#] hours.
+# I worked on this challenge [by myself].
+# This challenge took me [1] hours.
 
 # Pseudocode
-
-
+# 	Split the sentence into individual strings
+# 	Reverse the strings
+# 	Print the sentence back out
 
 # Initial Solution
+def reverse_words(sentence)
+	sent_arr = sentence.split(" ")
+	sent_arr.each do |word|
+		sent_arr[sent_arr.index(word)] = word.reverse
+	end
+	return sent_arr.join(" ")
+end
 
-
-
-
-# Refactored Solution
-
-
-
-
-
-# Reflection
